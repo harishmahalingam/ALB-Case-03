a29c4c9e846e61d95eeee9f5eaf3e21808e7934d
harishmahalingam <129191989+harishmahalingam@users.noreply.github.com>
2025-04-16 11:05:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,58 @@
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
 
+  name    = "my-alb"
+  vpc_id  = "vpc-02d71941e8e649181"
+  subnets = ["subnet-03644421210c068be2", "subnet-09b83ca88f6d2ab35"]
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+#   all_https = {
+#     from_port   = 443
+#      to_port     = 443
+#      ip_protocol = "tcp"
+#     description = "HTTPS web traffic"
+#      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = "i-0f6d38a07d50d080f"
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+}
